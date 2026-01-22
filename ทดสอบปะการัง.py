9af1d623730825e983dca45ac1188740b8a49dad
NewbieOhm <166592888+NewbieOhm@users.noreply.github.com>
2026-01-22 21:18:13 +0700

    Add files via upload

---
+++ ทดสอบปะการัง.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("🌊 ReefRevive")
+st.write("AI for Coral Reef Restoration")
+
+st.write("### What does this app do?")
+st.write("This app demonstrates how AI helps monitor coral reefs.")
+
+st.button("Start Demo")
